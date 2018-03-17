7ebb8aa1c9d43d59a33b61fb2bb81f6bdc0c9cb5
stevegbrooks <stevegbrooks@gmail.com>
2018-03-17 16:31:43 -0400

    finished Dictionary class, but still need to write tests for it

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,4 +1,41 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class Dictionary {
-
+	
+	private String fileName;
+	private MyHashSet<String> wordSet;
+	
+	public Dictionary(String fileName) {
+		this.fileName = fileName;
+		wordSet = new MyHashSet<>();
+		readFile();
+	}
+	
+	private void readFile() {
+		try {
+			File inputFile = new File(fileName);
+			Scanner in = new Scanner(inputFile, "utf-8");
+			while (in.hasNextLine()) {
+				String word = in.nextLine();
+				word = word.toLowerCase();
+				wordSet.add(word);
+			}
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * returns true if the dictionary file contains the 
+	 * string passed to this method (case-insensitive),
+	 * and false otherwise.
+	 * @param s any string
+	 * @return true or false
+	 */
+	public boolean contains(String s) {
+		String wordToCheck = s.toLowerCase();
+		return wordSet.contains(wordToCheck);
+	}
 }
